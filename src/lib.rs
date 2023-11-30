664c5af8612f42c8e5caa3979a6d9e2e0a555de3
Anuraj Tapidas <atapidas@gmail.com>
2023-11-30 18:26:01 +0000

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use std::env;
+
+use dotenv;
+use reqwest::blocking::Client;
+use reqwest::Method;
+
+pub fn download_day(year: u16, day: u8) -> String {
+    dotenv::dotenv().unwrap();
+    let cookie = env::var("COOKIE").expect("Expected cookie");
+    let url = format!("https://adventofcode.com/{year}/day/{day}/input");
+    let client = Client::new();
+    let response = client
+        .request(Method::GET, url)
+        .header("Cookie", format!("session={cookie}"))
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
+
+    response
+}
+
+#[cfg(test)]
+mod tests {
+    use super::download_day;
+
+    #[test]
+    fn sample_test() {
+        println!("{}", download_day(2015, 7));
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Hello, welcome to Anuraj's Advent of Code 2023! \nGo to any of the days and run the tests to see the results for each part :)");
+}
