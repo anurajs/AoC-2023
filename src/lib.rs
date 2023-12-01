af7ab81a4ca2dcb63340b48257af8c8ecab45b1d
Anuraj Tapidas <anutapid@publicisgroupe.net>
2023-12-01 09:59:26 +0000

    fixed typo

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+pub mod day_1;
+
 use std::env;
 
 use dotenv;
@@ -6,7 +8,7 @@ use reqwest::Method;
 
 pub fn download_day(year: u16, day: u8) -> String {
     dotenv::dotenv().unwrap();
-    let cookie = env::var("AOC_COOKI").expect("Expected cookie");
+    let cookie = env::var("AOC_COOKIE").expect("Expected cookie");
     let url = format!("https://adventofcode.com/{year}/day/{day}/input");
     let client = Client::new();
     let response = client
