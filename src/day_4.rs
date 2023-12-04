019ae251b27f2f6d3d4c70c12f80684dde3b4e22
Anuraj Tapidas <anutapid@publicisgroupe.net>
2023-12-04 11:02:07 +0000

    day 4

---
+++ src/day_4.rs
@@ -0,0 +1,117 @@
+use std::collections::HashSet;
+
+pub fn calculate_matching(card: &str) -> u32 {
+    let sides: Vec<&str> = card.split("|").collect();
+    let winning_side = sides[0];
+    let winning_side = winning_side.split(":").collect::<Vec<&str>>()[1];
+    let picked_side = sides[1];
+    let mut winning_iter = winning_side.split_whitespace();
+
+    let mut winning: HashSet<usize> = HashSet::new();
+    while let Some(num) = winning_iter.next() {
+        if num.trim().is_empty() {
+            continue;
+        }
+        winning.insert(num.parse().expect("expected number"));
+    }
+
+    let mut matching = 0;
+    let mut picked_iter = picked_side.split_whitespace();
+    while let Some(num) = picked_iter.next() {
+        if num.trim().is_empty() {
+            continue;
+        }
+        let num = num.parse().expect("expected number");
+        if winning.contains(&num) {
+            matching += 1;
+        }
+    }
+
+    matching
+}
+
+pub fn calculate_copies(cards: &str) -> u32 {
+    let score: Vec<u32> = cards.lines().map(calculate_matching).collect();
+    let mut copies = vec![1; score.len()];
+    for i in 0..copies.len() {
+        let amount = copies[i];
+        for j in i + 1..=i + score[i] as usize {
+            copies[j] += amount
+        }
+    }
+
+    copies.iter().sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::download_day;
+
+    use super::{calculate_copies, calculate_matching};
+
+    #[test]
+    fn part_one() {
+        let content = download_day(2023, 4);
+        let res: u32 = content
+            .lines()
+            .map(calculate_matching)
+            .map(|amount| {
+                if amount == 0 {
+                    0
+                } else {
+                    2_u32.pow(amount - 1)
+                }
+            })
+            .sum();
+        println!("Part 1: {res}");
+    }
+
+    #[test]
+    fn part_two() {
+        let content = download_day(2023, 4);
+        let res = calculate_copies(&content);
+        println!("Part two: {res}");
+    }
+
+    #[test]
+    fn score_test() {
+        let card = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
+        let res = calculate_matching(card);
+        assert_eq!(res, 8);
+    }
+
+    #[test]
+    fn copies_test() {
+        let content = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+        let res = calculate_copies(content);
+        assert_eq!(res, 30);
+    }
+
+    #[test]
+    fn calculate_pile() {
+        let content = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+        let res: u32 = content
+            .lines()
+            .map(calculate_matching)
+            .map(|amount| {
+                if amount == 0 {
+                    0
+                } else {
+                    2_u32.pow(amount - 1)
+                }
+            })
+            .sum();
+        assert_eq!(res, 13);
+    }
+}
