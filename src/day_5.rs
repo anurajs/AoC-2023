519a7f0fc086443f79dad19bc5d18df054c8cbda
Anuraj Tapidas <anutapid@publicisgroupe.net>
2023-12-05 18:26:32 +0000

    calculate location ranges

--- src/day_5.rs
+++ src/day_5.rs
@@ -7,7 +7,7 @@ pub struct Map {
     range: usize,
 }
 
-#[derive(Debug, Hash, PartialEq, Eq)]
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
 pub struct LocationRange {
     location: usize,
     range: usize,
@@ -19,6 +19,125 @@ pub fn is_in_range(map: &Map, initial: usize) -> Option<usize> {
         return Some(location);
     }
     None
+}
+
+pub fn ranges_in_map(
+    location_range: &LocationRange,
+    map: &Map,
+) -> (HashSet<LocationRange>, HashSet<LocationRange>) {
+    let mut covered = HashSet::new();
+    let mut not_covered = HashSet::new();
+    if location_range.location < map.source {
+        not_covered.insert(LocationRange {
+            location: location_range.location,
+            range: (location_range.range).min(map.source - location_range.location),
+        });
+        if map.source - location_range.location < location_range.range {
+            covered.insert(LocationRange {
+                location: map.source,
+                range: map
+                    .range
+                    .min(location_range.location + location_range.range - map.source),
+            });
+            if map.source + map.range < location_range.location + location_range.range {
+                not_covered.insert(LocationRange {
+                    location: map.source + map.range,
+                    range: location_range.location + location_range.range - map.source - map.range,
+                });
+            }
+        };
+    }
+
+    if location_range.location >= map.source && location_range.location < map.source + map.range {
+        covered.insert(LocationRange {
+            location: location_range.location,
+            range: location_range
+                .range
+                .min(map.source + map.range - location_range.location),
+        });
+        if location_range.range > map.source + map.range - location_range.location {
+            not_covered.insert(LocationRange {
+                location: map.source + map.range,
+                range: location_range.range + location_range.location - map.source - map.range,
+            });
+        }
+    } else {
+        not_covered.insert(location_range.clone());
+    }
+
+    (covered, not_covered)
+}
+
+fn is_range_covered(range: &LocationRange, map: &Map) -> Option<LocationRange> {
+    if range.location >= map.source && range.location + range.range <= map.source + map.range {
+        return Some(LocationRange {
+            location: map.destination + (range.location - map.source),
+            range: range.range,
+        });
+    }
+
+    None
+}
+
+pub fn calculate_location_ranges(
+    initial: HashSet<LocationRange>,
+    maps: &Vec<&str>,
+) -> HashSet<LocationRange> {
+    let mut map_collection: Vec<Map> = Vec::new();
+    for line in maps {
+        let mut parts = line.split_whitespace();
+        let destination = parts.next().unwrap().parse().unwrap();
+        let source = parts.next().unwrap().parse().unwrap();
+        let range = parts.next().unwrap().parse().unwrap();
+        map_collection.push(Map {
+            source,
+            range,
+            destination,
+        });
+    }
+
+    let mut covered: HashSet<LocationRange> = HashSet::new();
+    let mut not_covered: HashSet<LocationRange> = HashSet::from(initial);
+    loop {
+        let num_uncovered = not_covered.len();
+        let mut temp_not_covered = HashSet::new();
+        for uncovered in not_covered.drain() {
+            for map in map_collection.iter() {
+                let (mut c, mut u) = ranges_in_map(&uncovered, map);
+                covered.extend(c.drain());
+                temp_not_covered.extend(u.drain());
+            }
+        }
+        not_covered = temp_not_covered.difference(&covered).cloned().collect();
+        if not_covered.len() == num_uncovered {
+            break;
+        }
+    }
+
+    let mut covered_to_min: HashMap<&LocationRange, LocationRange> = HashMap::new();
+    for c in covered.iter() {
+        for map in map_collection.iter() {
+            match is_range_covered(c, map) {
+                Some(location) => {
+                    covered_to_min
+                        .entry(c)
+                        .and_modify(|c| {
+                            if location.location < c.location {
+                                c.location = location.location;
+                                c.range = location.range;
+                            }
+                        })
+                        .or_insert(location);
+                }
+                None => {}
+            }
+        }
+    }
+
+    // not_covered.extend(covered_to_min.values())
+    let min_covered: HashSet<LocationRange> = covered_to_min.into_values().collect();
+    not_covered.extend(min_covered);
+    not_covered
 }
 
 pub fn calculate_location(initial: HashSet<usize>, maps: &Vec<&str>) -> HashSet<usize> {
@@ -61,13 +180,16 @@ pub fn calculate_location(initial: HashSet<usize>, maps: &Vec<&str>) -> HashSet<
 mod tests {
     use std::collections::HashSet;
 
-    use crate::{day_5::calculate_location, download_day};
+    use crate::{
+        day_5::{calculate_location, calculate_location_ranges, LocationRange},
+        download_day,
+    };
 
     #[test]
     fn part_two() {
         let content = download_day(2023, 5);
         let mut line_iter = content.lines();
-        let mut ranges: Vec<usize> = line_iter
+        let ranges: Vec<usize> = line_iter
             .next()
             .unwrap()
             .split(": ")
@@ -77,10 +199,11 @@ mod tests {
             .map(|v| v.parse().unwrap())
             .collect();
         let mut initial = HashSet::new();
-        for x in ranges.windows(2) {
-            for i in x[0]..x[0] + x[1] {
-                initial.insert(i);
-            }
+        for x in ranges.windows(2).step_by(2) {
+            initial.insert(LocationRange {
+                location: x[0],
+                range: x[1],
+            });
         }
 
         let mut maps: Vec<&str> = Vec::new();
@@ -90,14 +213,19 @@ mod tests {
             if !line.trim().is_empty() {
                 maps.push(line);
             } else {
-                initial = calculate_location(initial, &maps);
-                maps.clear();
+                initial = calculate_location_ranges(initial, &maps);
+                println!("{initial:?}");
                 line_iter.next();
+                maps.clear();
             }
         }
 
-        let res = calculate_location(initial, &maps);
-        println!("Part 2: {}", res.iter().min().unwrap())
+        let res = calculate_location_ranges(initial, &maps);
+
+        println!(
+            "Part 2: {}",
+            res.iter().fold(usize::MAX, |acc, x| acc.min(x.location))
+        )
     }
 
     #[test]
@@ -203,5 +331,80 @@ humidity-to-location map:
 
         let res = calculate_location(initial, &maps);
         assert_eq!(res, HashSet::from([82, 43, 86, 35]));
+    }
+
+    #[test]
+    fn part_two_sample() {
+        let content = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+
+        let mut line_iter = content.lines();
+        let ranges: Vec<usize> = line_iter
+            .next()
+            .unwrap()
+            .split(": ")
+            .nth(1)
+            .unwrap()
+            .split_whitespace()
+            .map(|v| v.parse().unwrap())
+            .collect();
+        let mut initial = HashSet::new();
+        for x in ranges.windows(2).step_by(2) {
+            initial.insert(LocationRange {
+                location: x[0],
+                range: x[1],
+            });
+        }
+
+        let mut maps: Vec<&str> = Vec::new();
+        line_iter.next();
+        line_iter.next();
+        while let Some(line) = line_iter.next() {
+            if !line.trim().is_empty() {
+                maps.push(line);
+            } else {
+                initial = calculate_location_ranges(initial, &maps);
+                line_iter.next();
+                maps.clear();
+            }
+        }
+
+        let res = calculate_location_ranges(initial, &maps);
+
+        println!(
+            "Part 2: {}",
+            res.iter().fold(usize::MAX, |acc, x| acc.min(x.location))
+        )
     }
 }
